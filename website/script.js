e18881c958490c4a284d0799e44ff58802b0dfa0
Bouncybull <vinceschoutrop@gmail.com>
2025-04-13 18:15:08 +0200

    g

--- website/script.js
+++ website/script.js
@@ -4,3 +4,27 @@ titlescreen.addEventListener("click", function(event)
 {
 
 })
+
+async function getData() {
+    const url = "./dialogue.json";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+
+      /* let text = document.createElement("p");
+      text.innerHTML = json.location.name + " " + json.location.localtime;
+      document.getElementById("this").appendChild(text);
+
+      let text2 = document.createElement("p");
+      text2.innerHTML = json.current.temperature + "°C";
+      document.getElementById("this").appendChild(text2); */
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
